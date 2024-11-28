12cabcda082b5504ea6270fae3fd3a3c6ce7a769
deni9309 <deni9309@gmail.com>
2024-11-28 19:46:16 +0200

    implement createUser method; update prisma schema and user model id to be cuid

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Post, UseInterceptors } from '@nestjs/common'
 import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UsersService } from './users.service'
+import { NoFilesInterceptor } from '@nestjs/platform-express'
 
 @ApiTags('users')
 @Controller('/api/users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @ApiCreatedResponse({description: "The user has been successfully created."})
+  @ApiCreatedResponse({
+    description: 'The user has been successfully created.',
+  })
   @Post()
+  @UseInterceptors(NoFilesInterceptor())
   createUser(@Body() user: CreateUserDto) {
     return this.usersService.createUser(user)
   }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,30 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, UnprocessableEntityException } from '@nestjs/common'
+import * as bcrypt from 'bcrypt'
+
+import { PrismaService } from '../prisma/prisma.service'
 import { CreateUserDto } from './dto/create-user.dto'
 
 @Injectable()
 export class UsersService {
-  createUser(_data: CreateUserDto) {}
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(data: CreateUserDto): Promise<{ id: string; email: string }> {
+    try {
+      return await this.prismaService.user.create({
+        data: {
+          ...data,
+          password: await bcrypt.hash(data.password, 10),
+        },
+        select: {
+          id: true,
+          email: true,
+        },
+      })
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new UnprocessableEntityException('Email already in use.')
+      }
+      throw error
+    }
+  }
 }
